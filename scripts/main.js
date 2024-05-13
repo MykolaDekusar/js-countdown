152b764362a6ede8fffdb0cd26819b1bc49af94c
MykolaDekusar <ndekusar97@gmail.com>
2024-05-13 15:03:22 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const timer = document.getElementById('countDown');
+
+
+setTimeout(buonAnno, 10000);
+
+function buonAnno() {
+    const mostraBuonAnno = document.getElementById('buonAnno');
+    mostraBuonAnno.classList.remove('hide');
+}
